2edd23bb017959f399358a8f0567e3f6e30d5ac9
oskirove <oskirove@gmail.com>
2026-02-21 16:24:19 +0100

    feat(client): automatizar inyección de token JWT en Ktor
    Se ha implementado el plugin DefaultRequest en el KtorClient para incluir automáticamente el header de Authorization si el token existe en Settings. Simplificamos los repositorios al eliminra la necesidad de pasar el token manualmente en cada petición.

--- composeApp/src/commonMain/kotlin/app/takent/mobile/data/network/KtorClient.kt
+++ composeApp/src/commonMain/kotlin/app/takent/mobile/data/network/KtorClient.kt
@@ -1,8 +1,12 @@
 package app.takent.mobile.data.network
 
+import com.russhwolf.settings.Settings
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.header
+import io.ktor.http.HttpHeaders
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
@@ -20,6 +24,13 @@ object KtorClient {
             requestTimeoutMillis = 30000
             connectTimeoutMillis = 30000
             socketTimeoutMillis = 30000
+        }
+
+        install(DefaultRequest) {
+            val token = Settings().getStringOrNull("token")
+            if (token != null) {
+                header(HttpHeaders.Authorization, "Bearer $token")
+            }
         }
     }
 }
